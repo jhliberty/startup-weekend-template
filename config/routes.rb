efbd0dbbef4e26ebce1015a3e257cc3d312b293b
John-Henry Liberty <johnhenry.liberty@gmail.com>
2014-07-29 12:45:42 -0500

    Moves design out into partials, sets up a couple static pages, routes, title helper and tests for title helper and active page nav class

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Blogjhl::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/about"
+
+  get "/about", to: "static_pages#about", as: :about
+  root to: "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,16 +7,26 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the base title 'John-Henry Liberty'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{base_title}")
+    end
+
+    it "should have an active class on the home link" do
+      visit root_path
+      expect(page).to have_css("li.active a", text: "Home")
     end
   end
 
   describe "About page" do
 
   	it "should have the title 'About me' " do
-	  	visit '/static_pages/about'
+	  	visit about_path
 	  	expect(page).to have_title("About me | #{base_title}")
   	end
+
+    it "should have an active class on the about link" do
+      visit about_path
+      expect(page).to have_css("li.active a", text: "About")
+    end
   end
 end
