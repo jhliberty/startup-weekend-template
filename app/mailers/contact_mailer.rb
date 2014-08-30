8c9688ca4d07c74fe7653b041362e14e37463e9a
John-Henry Liberty <johnhenry.liberty@gmail.com>
2014-08-30 15:59:57 -0500

    Adds mandrill config for heroku

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: "your@email.com"
+  default to: "johnhenry.liberty@gmail.com"
 
   def contact_email(entry)
   	@entry = entry
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,15 @@ Blogjhl::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mandrillapp.com',
+    port:                 587,
+    domain:               'heroku.com',
+    user_name:            ENV['MANDRILL_USERNAME'],
+    password:             ENV['MANDRILL_APIKEY'],
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 end
